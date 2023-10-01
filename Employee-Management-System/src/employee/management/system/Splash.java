378b5066857c2602bf789aa6904f75b2894f5a2a
tejas-maloor <67369019+tejas-maloor@users.noreply.github.com>
2023-10-01 20:08:10 +0530

    Main
    Main Commit

---
+++ Employee-Management-System/src/employee/management/system/Splash.java
@@ -0,0 +1,64 @@
+package employee.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Splash extends JFrame implements ActionListener {
+    
+    Splash() {
+        
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("EMPLOYEE MANAGEMENT SYSTEM");
+        heading.setBounds(80, 30, 1200, 60);
+        heading.setFont(new Font("serif", Font.PLAIN, 60));
+        heading.setForeground(Color.RED);
+        add(heading);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/delete.png"));
+        Image i2 = i1.getImage().getScaledInstance(900, 500, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(50, 50, 1050, 500);
+        add(image);
+        
+        JButton clickhere = new JButton("CONTINUE");
+        clickhere.setBounds(930, 400, 120, 50);
+        clickhere.setBackground(Color.BLACK);
+        clickhere.setForeground(Color.WHITE);
+        clickhere.addActionListener(this);
+        image.add(clickhere);
+        
+        
+        setSize(1170, 600);
+        setLocation(200, 90);
+        setVisible(true);
+        
+        while(true) {
+            heading.setVisible(false);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e){
+                
+            }
+            
+            heading.setVisible(true);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e){
+                
+            }
+        }
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Login();
+    }
+    
+    public static void main(String args[]) {
+        new Splash();
+    }
+}
